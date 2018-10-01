98be3b8e2d91da5881a24d788a2634be41618300
haris00 <haristanvir@hotmail.com>
2018-09-30 23:23:21 -0230

    registering blue prints

--- classPlay/__init__.py
+++ classPlay/__init__.py
@@ -4,4 +4,10 @@ from classPlay.config import Config
 
 def create_app(config_class=Config):
     app = Flask(__name__)
+    from classPlay.main.routes import main
+    from classPlay.professor.routes import professor
+    from classPlay.student.routes import student
+    app.register_blueprint(professor)
+    app.register_blueprint(student)
+    app.register_blueprint(main)
     return app
---
+++ classPlay/professor/routes.py
@@ -0,0 +1,3 @@
+from flask import Blueprint
+
+professor = Blueprint('professor', __name__)
